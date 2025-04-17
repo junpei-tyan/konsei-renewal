e3b1e07147fa5824a7197d5387e10691cb9a6ef1
junpei-tyan <pei1441@gmail.com>
2025-04-17 15:27:35 +0900

    footerの作成

--- app/public/wp-content/themes/konsei-newtheme/header.php
+++ app/public/wp-content/themes/konsei-newtheme/header.php
@@ -5,6 +5,10 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="description" content="<?php bloginfo('description'); ?>">
   <title><?php echo bloginfo('name'); ?></title>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Noto+Serif+JP:wght@200..900&family=Pacifico&family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Noto+Serif+JP:wght@200..900&family=Pacifico&family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet">
 
   <?php wp_head(); ?>
 </head>
@@ -25,7 +29,7 @@
         </ul>
       </nav>
     </div>
-    <div class="header-sns-icons">
+    <div class="sns-icons">
       <a href="https://www.facebook.com/people/%E6%9C%89%E3%81%93%E3%82%93%E3%81%9B%E3%81%84/100057582454245/">
         <img src="<?php echo get_template_directory_uri(); ?>/assets/images/icon/facebook-icon.svg" alt="facebook-icon">
       </a>
@@ -37,5 +41,3 @@
       <button>JP / EN</button>
     </div>
   </header>
-</body>
-</html>
