4eab5e1e7a8c208063193b5489c253968ee14b2a
tahoito <taho.ito0304@gmail.com>
2025-04-20 23:19:24 +0900

    page-about edit

--- app/public/wp-content/themes/konsei-newtheme/functions.php
+++ app/public/wp-content/themes/konsei-newtheme/functions.php
@@ -7,17 +7,5 @@
     wp_enqueue_script("script",get_template_directory_uri()."/assets/js/script.js",array(),false, true);
   }
   add_action('wp_enqueue_scripts','add_files');
-
-  function add_custom_about_styles() {
-    if (is_page_template('page-about.php')) {
-      wp_enqueue_style(
-        'page-about-style',
-        get_template_directory_uri() . '/assets/css/page-about.css',
-        array(),
-        null
-      );
-    }
-  }
-  add_action('wp_enqueue_scripts', 'add_custom_about_styles');
   
 ?>
