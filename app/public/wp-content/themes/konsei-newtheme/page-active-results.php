ae0f34b84657b50619ca857bd5988e2cc6d8d3cc
tahoito <taho.ito0304@gmail.com>
2025-04-21 18:19:43 +0900

    make active_results

--- app/public/wp-content/themes/konsei-newtheme/page-active-results.php
+++ app/public/wp-content/themes/konsei-newtheme/page-active-results.php
@@ -8,4 +8,26 @@
 
 <!-- 【活動実績】ページ -->
 
+<main>
+  <div class="active-results-firstview">
+    <h1>活動実績</h1>
+  </div>
+
+  <div class="active-results_top">
+    <div class="active-results_intro">
+    人と人が絞り染めを通じてつながり、互いを認め合う。
+    <br>会話が生まれ、笑顔があふれ、新たな絆が育まれていく。
+    <br>絞り染めは、コミュニケーションのツールにもなり得るのです。
+    私たちは、伝統工芸の新たな可能性を体験を通じて提案しています。
+    </div>
+  </div>
+
+  <div class="results_card">
+    <div class="results_title"> 
+    </div>
+  </div>
+
+</main>
+
+
 <?php get_footer(); ?>
