bb307431b1f9de0ff6f9c9baed4571c428a421dc
junpei-tyan <pei1441@gmail.com>
2025-04-24 13:49:56 +0900

    Guest Page の作成

--- app/public/wp-content/themes/konsei-newtheme/page-guest.php
+++ app/public/wp-content/themes/konsei-newtheme/page-guest.php
@@ -1,13 +1,177 @@
 <?php
   /*
-  Template Name: Guest Page
+  Template Name: Experience Page
   */
 
   get_header(); 
 ?>
 
+<!-- 【お客様】ページ -->
+<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/page-guest.css">
 <main>
-  <h1>Guest Page</h1>
+    
+    <div class="about-firstview">
+        <h1>お客様ページ</h1>
+    </div>
+
+    <div class="customer-section">
+        <div class="section__title">
+        ======== お客様の声 ======== 
+        </div>
+
+        <div class="voice-item">
+            <div class="icon">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/guest/icon.png">
+            </div>
+            <div class="voice-text">
+            <p>色や柄を何にしようか みんなとワイワイ楽しく話しながらできたのと、<br>一人一人全く一緒のものはなくて唯一無二の反物が作れた　ゆかた作って着るのが楽しみ！</p>
+            </div>
+        </div>
+        
+        <div class="voice-item">
+            <div class="icon">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/guest/icon.png">
+            </div>
+            <div class="voice-text">
+            <p>色や柄を何にしようか みんなとワイワイ楽しく話しながらできたのと、<br>一人一人全く一緒のものはなくて唯一無二の反物が作れた　ゆかた作って着るのが楽しみ！</p>
+            </div>
+        </div>
+
+        <div class="voice-item">
+            <div class="icon">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/guest/icon.png">
+            </div>
+            <div class="voice-text">
+            <p>色んな技法、工夫があるのだなと思いました。<br>発色の出具合は本当にきれいでした。ありがとうございました。</p>
+            </div>
+        </div>
+
+        <div class="voice-item">
+            <div class="icon">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/guest/icon.png">
+            </div>
+            <div class="voice-text">
+            <p>大変だったけど、やりがいがあって楽しかったです。<br>自分で作った生地で浴衣ができるのがうれしいです。</p>
+            </div>
+        </div>
+
+        <div class="voice-item">
+            <div class="icon">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/guest/icon.png">
+            </div>
+            <div class="voice-text">
+            <p>想像以上の楽しさ、また絞り染めの奥深さを知ることができました。<br>
+                丁寧なご指導に感謝申し上げます。<br>
+                海外の友人が日本に来た際にも紹介したいと思いました。</p>
+            </div>
+        </div>
+
+        <div class="voice-item">
+            <div class="icon">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/guest/icon.png">
+            </div>
+            <div class="voice-text">
+            <p>一反丸々染めさせてくれるというのにまず驚きました。<br>
+            自分の手で折って染めたものを身につけることのできる嬉しさと言ったらありません。</p>
+            </div>
+        </div>
+
+    
+
+        <button class="button more">もっと見る</button>
+
+    </div>
+
+    <div class="photo-section">
+        <div class="section__title">
+        ======== 体験に参加された皆さんの雪花絞り ========
+        </div>
+
+        <div class="photo-box">
+            <div class="image-grid">
+                <div class="image-item">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/guest/photo1.png">
+                </div>
+                <div class="image-item">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/guest/photo2.png">
+                </div>
+                <div class="image-item">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/guest/photo3.png">
+                </div>
+                <div class="image-item">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/guest/photo4.png">
+                </div>
+                <div class="image-item">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/guest/photo5.png">
+                </div>
+                <div class="image-item">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/guest/photo6.png">
+                </div>
+                <div class="image-item">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/guest/photo7.png">
+                </div>
+                <div class="image-item">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/guest/photo8.png">
+                </div>
+                <div class="image-item">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/guest/photo9.png">
+                </div>
+            </div>
+
+            <button class="button more">もっと見る</button>
+
+            <div class="sns-section">
+                <div class="comment">
+                    <p>みなさんとても素敵な写真だワン！</p>
+                    <p>InstagramやFacebookにも<br>素敵なお写真がいっぱいあるワン！</p>
+                </div>
+                
+                <div class="dog">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/guest/dog.png">
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="movie-section">
+        <div class="section__title">
+            ======== 体験参考動画 ========
+        </div>
+        <div class="movie_box">
+            <!-- 1つ目のYouTube動画 -->
+            <div class="movie_item">
+                <iframe src="https://www.youtube.com/embed/rSW03hFJu2k?si=fTquWDX56WbHFhSc" 
+                        title="YouTube video player" 
+                        frameborder="0" 
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+                        referrerpolicy="strict-origin-when-cross-origin" 
+                        allowfullscreen>
+                </iframe>
+                <p>ワークショップにご参加いただきましたお客様より動画をご提供いただきました。<br>
+                ワークショップの様子が分かりやすく、楽しく撮影されていましたので、許可をいただき掲載させていただきました。<br>
+                ※動画制作承ります</p>
+
+            </div>
+            <div class="movie_item">
+                <iframe src="https://www.youtube.com/embed/rSW03hFJu2k?si=fTquWDX56WbHFhSc" 
+                        title="YouTube video player" 
+                        frameborder="0" 
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+                        referrerpolicy="strict-origin-when-cross-origin" 
+                        allowfullscreen>
+                </iframe>
+                <p>ワークショップにご参加いただきましたお客様より動画をご提供いただきました。<br>
+                雪花絞りで反物を染色する過程から、ご自身で着られて、有松のまちで撮影されたお姿がとても素敵です。<br>
+                ご参加いただいた皆様の笑顔やキラキラしている様子がとっても美しくてかわいくて、私たちの心も躍りました。<br>
+                （企画 緑区で活動している、弾む和裁士tomomi  ／ 動画制作 あすきゃん ）※動画制作承ります</p>
+            </div>
+        </div>
+    </div>
+    <div class="border"></div>
+
 </main>
+
+
 
 <?php get_footer(); ?>
